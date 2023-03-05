7a61968c5a7b6ec8a6c09070eb38cc40fbfe02a3
sajjad <sajjadheydari1401@gmail.com>
2023-03-05 11:18:18 +0330

    fixed products scrolling

--- lib/screens/product/product_screen.dart
+++ lib/screens/product/product_screen.dart
@@ -96,184 +96,200 @@ class _ProductScreenState extends State<ProductScreen> {
     return Scaffold(
       appBar: const CustomAppBar(title: 'افزودن غذا'),
       bottomNavigationBar: const CustomNavBar(currentTabIndex: 0),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 20),
-                width: 400,
-                padding: const EdgeInsets.all(16.0),
-                child: Form(
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(
-                        height: 50,
-                        child: TextField(
-                          onChanged: (value) {
-                            setState(() {
-                              _isFormValid = isFormValid();
-                            });
-                          },
-                          style: const TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                          controller: _titleController,
-                          textAlign: TextAlign.center,
-                          keyboardType: TextInputType.name,
-                          decoration: const InputDecoration(
-                            hintText: 'نام غذا',
-                            contentPadding: EdgeInsets.only(top: 10),
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      SizedBox(
-                        height: 50,
-                        child: TextField(
-                          onChanged: (value) {
-                            setState(() {
-                              _isFormValid = isFormValid();
-                            });
-                          },
-                          style: const TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                          controller: _priceController,
-                          textAlign: TextAlign.center,
-                          keyboardType: TextInputType.number,
-                          decoration: const InputDecoration(
-                            hintText: 'قیمت',
-                            contentPadding: EdgeInsets.only(top: 10),
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      SizedBox(
-                        width: 200,
-                        height: 50,
-                        child: IgnorePointer(
-                          ignoring: !isFormValid(),
-                          child: ElevatedButton(
-                            onPressed: isFormValid()
-                                ? () => _saveProduct(context)
-                                : null,
-                            child: const Text(
-                              'افزودن محصول',
-                              style: TextStyle(
+      body: Center(
+        child: Container(
+          width: 500,
+          child: SingleChildScrollView(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: [
+                  Container(
+                    width: 500,
+                    margin: const EdgeInsets.only(top: 20),
+                    padding: const EdgeInsets.all(16.0),
+                    child: Form(
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 50,
+                            child: TextField(
+                              onChanged: (value) {
+                                setState(() {
+                                  _isFormValid = isFormValid();
+                                });
+                              },
+                              style: const TextStyle(
                                   fontSize: 18, fontWeight: FontWeight.bold),
+                              controller: _titleController,
+                              textAlign: TextAlign.center,
+                              keyboardType: TextInputType.name,
+                              decoration: const InputDecoration(
+                                hintText: 'نام غذا',
+                                contentPadding: EdgeInsets.only(top: 10),
+                                border: OutlineInputBorder(),
+                              ),
                             ),
                           ),
-                        ),
+                          const SizedBox(height: 10),
+                          SizedBox(
+                            height: 50,
+                            child: TextField(
+                              onChanged: (value) {
+                                setState(() {
+                                  _isFormValid = isFormValid();
+                                });
+                              },
+                              style: const TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                              controller: _priceController,
+                              textAlign: TextAlign.center,
+                              keyboardType: TextInputType.number,
+                              decoration: const InputDecoration(
+                                hintText: 'قیمت',
+                                contentPadding: EdgeInsets.only(top: 10),
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          SizedBox(
+                            width: 200,
+                            height: 50,
+                            child: IgnorePointer(
+                              ignoring: !isFormValid(),
+                              child: ElevatedButton(
+                                onPressed: isFormValid()
+                                    ? () => _saveProduct(context)
+                                    : null,
+                                child: const Text(
+                                  'افزودن محصول',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              _isLoading
-                  ? const Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : products.items.isEmpty
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  if (products.items.isNotEmpty)
+                    const Text(
+                      'برای حذف محصول، آیتم را به طرف چپ بکشید',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  _isLoading
                       ? const Center(
-                          child: Text(
-                            'غذایی ثبت نشده',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
+                          child: CircularProgressIndicator(),
                         )
-                      : SingleChildScrollView(
-                          child: SizedBox(
-                            width: 400,
-                            child: ListView.builder(
-                              shrinkWrap: true,
-                              itemCount: products.items.length,
-                              itemBuilder: (ctx, index) {
-                                final item = products.items[index];
+                      : products.items.isEmpty
+                          ? const Center(
+                              child: Text(
+                                'غذایی ثبت نشده',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            )
+                          : Expanded(
+                              child: SizedBox(
+                                width: 450,
+                                child: ListView.builder(
+                                  itemCount: products.items.length,
+                                  itemBuilder: (ctx, index) {
+                                    final item = products.items[index];
 
-                                return Dismissible(
-                                  key: Key(item.id),
-                                  confirmDismiss: (direction) async {
-                                    return await showDialog(
-                                      context: context,
-                                      builder: (BuildContext context) {
-                                        return AlertDialog(
-                                          title: const Text('حذف محصول'),
-                                          content: Text(
-                                              'آیا مطمئن هستید که می‌خواهید ${item.title} را حذف کنید؟'),
-                                          actions: <Widget>[
-                                            TextButton(
-                                              onPressed: () =>
-                                                  Navigator.of(context)
-                                                      .pop(false),
-                                              child: const Text('خیر'),
-                                            ),
-                                            TextButton(
-                                              onPressed: () =>
-                                                  Navigator.of(context)
-                                                      .pop(true),
-                                              child: const Text('بله'),
-                                            ),
-                                          ],
+                                    return Dismissible(
+                                      key: Key(item.id),
+                                      confirmDismiss: (direction) async {
+                                        return await showDialog(
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            return AlertDialog(
+                                              title: const Text('حذف محصول'),
+                                              content: Text(
+                                                  'آیا مطمئن هستید که می‌خواهید ${item.title} را حذف کنید؟'),
+                                              actions: <Widget>[
+                                                TextButton(
+                                                  onPressed: () =>
+                                                      Navigator.of(context)
+                                                          .pop(false),
+                                                  child: const Text('خیر'),
+                                                ),
+                                                TextButton(
+                                                  onPressed: () =>
+                                                      Navigator.of(context)
+                                                          .pop(true),
+                                                  child: const Text('بله'),
+                                                ),
+                                              ],
+                                            );
+                                          },
                                         );
                                       },
-                                    );
-                                  },
-                                  onDismissed: (direction) {
-                                    _deleteProduct(products.items[index].id);
-                                    // Then show a snackbar.
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(
-                                        content: Text('${item.title} حذف شد'),
-                                      ),
-                                    );
-                                  },
-                                  child: Card(
-                                    color: Colors.greenAccent,
-                                    elevation: 5,
-                                    margin: const EdgeInsets.symmetric(
-                                      horizontal: 15,
-                                      vertical: 5,
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            '${formatPrice(products.items[index].price)} تومان',
-                                            style: const TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold,
-                                            ),
+                                      onDismissed: (direction) {
+                                        _deleteProduct(
+                                            products.items[index].id);
+                                        // Then show a snackbar.
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(
+                                          SnackBar(
+                                            content:
+                                                Text('${item.title} حذف شد'),
                                           ),
-                                          Row(
+                                        );
+                                      },
+                                      child: Card(
+                                        color: Colors.greenAccent,
+                                        elevation: 5,
+                                        margin: const EdgeInsets.symmetric(
+                                          horizontal: 15,
+                                          vertical: 5,
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
                                             children: [
                                               Text(
-                                                products.items[index].title,
+                                                '${formatPrice(products.items[index].price)} تومان',
                                                 style: const TextStyle(
                                                   fontSize: 18,
                                                   fontWeight: FontWeight.bold,
                                                 ),
                                               ),
+                                              Row(
+                                                children: [
+                                                  Text(
+                                                    products.items[index].title,
+                                                    style: const TextStyle(
+                                                      fontSize: 18,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
                                             ],
                                           ),
-                                        ],
+                                        ),
                                       ),
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                        ),
-            ],
+                                    );
+                                  },
+                                ),
+                              ),
+                            )
+                ],
+              ),
+            ),
           ),
         ),
       ),
